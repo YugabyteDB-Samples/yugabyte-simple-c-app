75a68a13975fef9e38985bfc6ee3faf0772822cc
Denis Magda <dmagda@gridgain.com>
2022-07-18 12:35:19 -0400

    initialized connection parameters with defaults that work with a local cluster

--- sample-app.c
+++ sample-app.c
@@ -3,48 +3,58 @@
 #include <string.h>
 #include "libpq-fe.h"
 
-#define HOST ""
+#define HOST "127.0.0.1"
 #define PORT "5433"
 #define DB_NAME "yugabyte"
-#define USER ""
-#define PASSWORD ""
-#define SSL_MODE "verify-full"
-#define SSL_ROOT_CERT ""
+#define USER "yugabyte"
+#define PASSWORD "yugabyte"
 
+// Ucomment and define the SSL settings for YugabyteDB Managed and other secured deployments
+// #define SSL_MODE ""
+// #define SSL_ROOT_CERT ""
+
+#ifdef SSL_MODE
 #define CONN_STR "host=" HOST " port=" PORT " dbname=" DB_NAME \
                  " user=" USER " password=" PASSWORD \
                  " sslmode=" SSL_MODE " sslrootcert=" SSL_ROOT_CERT
+#else
+#define CONN_STR "host=" HOST " port=" PORT " dbname=" DB_NAME \
+                 " user=" USER " password=" PASSWORD
+#endif
 
-PGconn* connect();
+PGconn *connect();
 void createDatabase(PGconn *conn);
 void selectAccounts(PGconn *conn);
 void printErrorAndExit(PGconn *conn, PGresult *res);
 void transferMoneyBetweenAccounts(PGconn *conn);
 
-int main(int argc, char **argv) {
-    PGconn  *conn;
+int main(int argc, char **argv)
+{
+    PGconn *conn;
 
     conn = connect();
     createDatabase(conn);
     selectAccounts(conn);
     transferMoneyBetweenAccounts(conn);
     selectAccounts(conn);
-    
+
     PQfinish(conn);
 
     return 0;
 }
 
-PGconn* connect() {
+PGconn *connect()
+{
     PGconn *conn;
 
     printf(">>>> Connecting to YugabyteDB!\n");
 
     PQinitSSL(1);
-    
+
     conn = PQconnectdb(CONN_STR);
 
-    if (PQstatus(conn) != CONNECTION_OK) {
+    if (PQstatus(conn) != CONNECTION_OK)
+    {
         printErrorAndExit(conn, NULL);
     }
 
@@ -53,12 +63,14 @@ PGconn* connect() {
     return conn;
 }
 
-void createDatabase(PGconn *conn) {
+void createDatabase(PGconn *conn)
+{
     PGresult *res;
 
     res = PQexec(conn, "DROP TABLE IF EXISTS DemoAccount");
 
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
         printErrorAndExit(conn, res);
     }
 
@@ -68,8 +80,9 @@ void createDatabase(PGconn *conn) {
                         age int, \
                         country varchar, \
                         balance int)");
-    
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
         printErrorAndExit(conn, res);
     }
 
@@ -77,7 +90,8 @@ void createDatabase(PGconn *conn) {
                         (1, 'Jessica', 28, 'USA', 10000), \
                         (2, 'John', 28, 'Canada', 9000)");
 
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
         printErrorAndExit(conn, res);
     }
 
@@ -86,7 +100,8 @@ void createDatabase(PGconn *conn) {
     printf(">>>> Successfully created table DemoAccount.\n");
 }
 
-void selectAccounts(PGconn *conn) {
+void selectAccounts(PGconn *conn)
+{
     PGresult *res;
     int i;
 
@@ -94,38 +109,45 @@ void selectAccounts(PGconn *conn) {
 
     res = PQexec(conn, "SELECT name, age, country, balance FROM DemoAccount");
 
-    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+    if (PQresultStatus(res) != PGRES_TUPLES_OK)
+    {
         printErrorAndExit(conn, res);
     }
-    
-    for (i = 0; i < PQntuples(res); i++) {
-      printf("name = %s, age = %s, country = %s, balance = %s\n", 
-          PQgetvalue(res, i, 0), PQgetvalue(res, i, 1), PQgetvalue(res, i, 2), PQgetvalue(res, i, 3));
+
+    for (i = 0; i < PQntuples(res); i++)
+    {
+        printf("name = %s, age = %s, country = %s, balance = %s\n",
+               PQgetvalue(res, i, 0), PQgetvalue(res, i, 1), PQgetvalue(res, i, 2), PQgetvalue(res, i, 3));
     }
 
     PQclear(res);
 }
 
-void transferMoneyBetweenAccounts(PGconn *conn) {
+void transferMoneyBetweenAccounts(PGconn *conn)
+{
     PGresult *res;
 
     res = PQexec(conn, "BEGIN TRANSACTION");
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
         printErrorAndExit(conn, res);
     }
 
     res = PQexec(conn, "UPDATE DemoAccount SET balance = balance - 800 WHERE name = \'Jessica\'");
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
         printErrorAndExit(conn, res);
     }
 
     res = PQexec(conn, "UPDATE DemoAccount SET balance = balance + 800 WHERE name = \'John\'");
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
         printErrorAndExit(conn, res);
     }
 
     res = PQexec(conn, "COMMIT");
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
         printErrorAndExit(conn, res);
     }
 
@@ -134,14 +156,17 @@ void transferMoneyBetweenAccounts(PGconn *conn) {
     printf(">>>> Transferred 800 between accounts\n");
 }
 
-void printErrorAndExit(PGconn *conn, PGresult *res) {
+void printErrorAndExit(PGconn *conn, PGresult *res)
+{
     char *errCode;
 
-    if (res) {
+    if (res)
+    {
         errCode = PQresultErrorField(res, PG_DIAG_SQLSTATE);
 
         // Applies to logic of the transferMoneyBetweenAccounts method
-        if (errCode && strcmp(errCode, "40001")) {
+        if (errCode && strcmp(errCode, "40001"))
+        {
             printf("The operation is aborted due to a concurrent transaction that is modifying the same set of rows. \
                     Consider adding retry logic for production-grade applications.\n");
         }
@@ -154,4 +179,3 @@ void printErrorAndExit(PGconn *conn, PGresult *res) {
     PQfinish(conn);
     exit(1);
 }
-
